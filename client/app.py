48bda46283c81f292c3e2c47f41b61608daed7dc
IDNatte <idnatte@gmail.com>
2023-05-10 19:54:52 +0800

    @IDNatte TO-Do assignment

--- client/app.py
+++ client/app.py
@@ -1,21 +1,23 @@
 from prober.sys_prober import SystemProberThread
 from uploader.uploader import UploaderThread
 
+import json
 import time
 import sys
+import os
 
 
 def main():
-    sys_probe = SystemProberThread("test.slg")
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    config = os.path.join(base_path, "config")
 
-    sys_probe.start()
+    # To-Do:
+    # check if config is available or no
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        sys_probe.stop()
-        sys_probe.join()
+    with open(os.path.join(config, "config.json"), "r") as settings:
+        config_obj = json.loads(settings)
+
+    print(config_obj)
 
 
 if __name__ == "__main__":
