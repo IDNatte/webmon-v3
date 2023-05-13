169a4b57fdd7e6b0b702928530599ad99793164f
IDNatte <idnatte@gmail.com>
2023-05-13 19:37:07 +0800

    (client)updating time upload and probe

--- client/app.py
+++ client/app.py
@@ -88,7 +88,7 @@ def main():
             try:
                 if check:
                     try:
-                        time.sleep(5)
+                        time.sleep(60)
                         job_run(logger, base_path)
 
                     except KeyboardInterrupt:
